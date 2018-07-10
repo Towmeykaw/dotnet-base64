f4626de6edc697e1c979c257ac2408365aeee218
Towmeykaw <tkindmark@gmail.com>
2018-07-10 23:28:56 +0200

    Add project files.

---
+++ dotnet-base64/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
