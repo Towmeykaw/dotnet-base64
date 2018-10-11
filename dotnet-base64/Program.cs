cf03b3018eeb6e03c3994cb85c8aa32542f36e2d
towmeykaw <tkindmark@gmail.com>
2018-10-11 11:52:07 +0200

    Added support for piping data

--- dotnet-base64/Program.cs
+++ dotnet-base64/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 namespace dotnet_base64
@@ -7,43 +6,39 @@ namespace dotnet_base64
     [Command(
         Name = "base64",
         FullName = "A .NET Core global tool to base64 encode/decode strings.")]
-    class Program
+    internal class Program
     {
-        [Required(ErrorMessage = "You must add a string")]
         [Argument(0, Description = "The string to encode/decode")]
         public string StringToProcess { get; }
+
         [Option(CommandOptionType.NoValue, Description = "Decode data. ",
             ShortName = "d", LongName = "decode")]
         public bool Decode { get; } = false;
+        
         public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
         
-        private int OnExecute(CommandLineApplication app, IConsole console)
+        private int OnExecute(IConsole console)
         {
             try
-            {   
-                if (!console.IsOutputRedirected)
-                    ClearCurrentConsoleLine();
-                if (Decode)
+            {
+                string data;
+                if (!console.IsInputRedirected)
                 {
-                    Console.WriteLine(Encoding.UTF8.GetString(Convert.FromBase64String(StringToProcess)));
+                    data = StringToProcess ?? "";
                 }
                 else
-                {                    
-                    Console.WriteLine(Convert.ToBase64String(Encoding.UTF8.GetBytes(StringToProcess)));
-                }                
+                {
+                    data = Console.In.ReadLine();
+                }
+                Console.WriteLine(Decode
+                        ? Encoding.UTF8.GetString(Convert.FromBase64String(data))
+                        : Convert.ToBase64String(Encoding.UTF8.GetBytes(data)));
                 return 0;
             }
-            catch (Exception e)
+            catch
             {
                 return 1;
             }
-        }
-        public static void ClearCurrentConsoleLine()
-        {
-            int currentLineCursor = Console.CursorTop;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.BufferWidth));
-            Console.SetCursorPosition(0, currentLineCursor);
         }
     }
 }
