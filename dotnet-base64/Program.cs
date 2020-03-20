16947cb5182237f2627d69fd4c905707fc9a10a3
Tommy Kindmark <tommy.kindmark@vasttrafik.se>
2020-03-20 21:50:33 +0100

    Added input and output file options

--- dotnet-base64/Program.cs
+++ dotnet-base64/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using McMaster.Extensions.CommandLineUtils;
 namespace dotnet_base64
@@ -9,12 +10,20 @@ namespace dotnet_base64
     internal class Program
     {
         [Argument(0, Description = "The string to encode/decode")]
-        public string StringToProcess { get; }
+        public string StringToProcess { get; } = "";
 
         [Option(CommandOptionType.NoValue, Description = "Decode data. ",
             ShortName = "d", LongName = "decode")]
         public bool Decode { get; } = false;
-        
+
+        [Option(CommandOptionType.SingleValue, Description = "Input file to read data from.",
+            ShortName = "f", LongName = "inputfile")]
+        public string InputFilePath { get; }
+
+        [Option(CommandOptionType.SingleValue, Description = "Output file to write data to.",
+            ShortName = "o", LongName = "outputfile")]
+        public string OutputFilePath { get; }
+
         public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
         
         private int OnExecute(IConsole console)
@@ -22,17 +31,28 @@ namespace dotnet_base64
             try
             {
                 string data;
-                if (!console.IsInputRedirected)
+                if (InputFilePath != null && File.Exists(InputFilePath))
                 {
-                    data = StringToProcess ?? "";
+                    data = File.ReadAllText(InputFilePath);
                 }
                 else
                 {
-                    data = Console.In.ReadLine();
+                    data = !console.IsInputRedirected ? StringToProcess : Console.In.ReadLine();
                 }
-                Console.WriteLine(Decode
+
+                if (OutputFilePath != null)
+                {   
+                    File.AppendAllText(OutputFilePath, Decode
                         ? Encoding.UTF8.GetString(Convert.FromBase64String(data))
                         : Convert.ToBase64String(Encoding.UTF8.GetBytes(data)));
+                }
+                else
+                {
+                    Console.WriteLine(Decode
+                        ? Encoding.UTF8.GetString(Convert.FromBase64String(data))
+                        : Convert.ToBase64String(Encoding.UTF8.GetBytes(data)));
+                }
+
                 return 0;
             }
             catch
